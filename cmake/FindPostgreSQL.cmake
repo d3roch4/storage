0bf34eeeb31a920b26d167b2226a1be1423be3fe
André <d3roch4@gmail.com>
2018-04-09 11:21:44 -0300

    a

--- cmake/FindPostgreSQL.cmake
+++ cmake/FindPostgreSQL.cmake
@@ -1,92 +1,37 @@
-# - Find PostgreSQL
-# Find the PostgreSQL includes and client library
-# This module defines
-#  POSTGRESQL_INCLUDE_DIR, where to find libpq-fe.h
-#  POSTGRESQL_LIBRARIES, libraries needed to use PostgreSQL
-#  POSTGRESQL_VERSION, if found, version of PostgreSQL
-#  POSTGRESQL_FOUND, if false, do not try to use PostgreSQL
+# Copyright (C) 2007-2009 LuaDist.
+# Created by Peter Kapec <kapecp@gmail.com>
+# Redistribution and use of this file is allowed according to the terms of the MIT license.
+# For details see the COPYRIGHT file distributed with LuaDist.
+#	Note:
+#		Searching headers and libraries is very simple and is NOT as powerful as scripts
+#		distributed with CMake, because LuaDist defines directories to search for.
+#		Everyone is encouraged to contact the author with improvements. Maybe this file
+#		becomes part of CMake distribution sometimes.
+
+# - Find sqlite3
+# Find the native SQLITE3 headers and libraries.
 #
-# Copyright (c) 2010, Mateusz Loskot, <mateusz@loskot.net>
-# Copyright (c) 2006, Jaroslaw Staniek, <js@iidea.pl>
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# SQLITE3_INCLUDE_DIRS	- where to find sqlite3.h, etc.
+# SQLITE3_LIBRARIES	- List of libraries when using sqlite.
+# SQLITE3_FOUND	- True if sqlite found.
 
-find_program(PG_CONFIG NAMES pg_config
-  PATHS
-  ${POSTGRESQL_ROOT}/bin
-  $ENV{POSTGRESQL_ROOT}/bin
-  /usr/bin
-  /usr/local/bin
-  $ENV{ProgramFiles}/PostgreSQL/*/bin
-  $ENV{ProgramW6432}/PostgreSQL/*/bin
-  $ENV{SystemDrive}/PostgreSQL/*/bin
-  DOC "Path to pg_config utility")
+# Look for the header file.
+FIND_PATH(POSTGRESQL_INCLUDE_DIR NAMES postgresql/libpq-fe.h)
 
-if(PG_CONFIG)
-  exec_program(${PG_CONFIG}
-    ARGS "--version"
-    OUTPUT_VARIABLE PG_CONFIG_VERSION)
+# Look for the library.
+FIND_LIBRARY(POSTGRESQL_LIBRARY NAMES pq)
 
-  if(${PG_CONFIG_VERSION} MATCHES "^[A-Za-z]+[ ](.*)$")
-    string(REGEX REPLACE "^[A-Za-z]+[ ](.*)$" "\\1" POSTGRESQL_VERSION "${PG_CONFIG_VERSION}")
-  endif()
+# Handle the QUIETLY and REQUIRED arguments and set PostgreSQL_FOUND to TRUE if all listed variables are TRUE.
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PostgreSQL DEFAULT_MSG POSTGRESQL_LIBRARY POSTGRESQL_INCLUDE_DIR)
 
-  exec_program(${PG_CONFIG}
-    ARGS "--includedir"
-    OUTPUT_VARIABLE PG_CONFIG_INCLUDEDIR)
+# Copy the results to the output variables.
+IF(PostgreSQL_FOUND)
+        SET(POSTGRESQL_LIBRARIES ${POSTGRESQL_LIBRARY})
+        SET(POSTGRESQL_INCLUDE_DIRS ${POSTGRESQL_INCLUDE_DIR})
+ELSE(PostgreSQL_FOUND)
+        SET(POSTGRESQL_LIBRARIES)
+        SET(POSTGRESQL_INCLUDE_DIRS)
+ENDIF(PostgreSQL_FOUND)
 
-  exec_program(${PG_CONFIG}
-    ARGS "--libdir"
-    OUTPUT_VARIABLE PG_CONFIG_LIBDIR)
-else()
-  set(POSTGRESQL_VERSION "unknown")
-endif()
-
-find_path(POSTGRESQL_INCLUDE_DIR libpq-fe.h
-  ${PG_CONFIG_INCLUDEDIR}
-  ${POSTGRESQL_ROOT}/include
-  $ENV{POSTGRESQL_ROOT}/include
-  /usr/include/server
-  /usr/include/pgsql/server
-  /usr/local/include/pgsql/server
-  /usr/include/postgresql
-  /usr/include/postgresql/server
-  /usr/include/postgresql/*/server
-  $ENV{ProgramFiles}/PostgreSQL/*/include
-  $ENV{ProgramW6432}/PostgreSQL/*/include
-  $ENV{SystemDrive}/PostgreSQL/*/include)
-
-find_library(POSTGRESQL_LIBRARIES NAMES pq libpq
-  PATHS
-  ${PG_CONFIG_LIBDIR}
-  ${POSTGRESQL_ROOT}/lib
-  $ENV{POSTGRESQL_ROOT}/lib
-  /usr/lib
-  /usr/local/lib
-  /usr/lib/postgresql
-  /usr/lib64
-  /usr/local/lib64
-  /usr/lib64/postgresql
-  $ENV{ProgramFiles}/PostgreSQL/*/lib
-  $ENV{SystemDrive}/PostgreSQL/*/lib
-  $ENV{ProgramFiles}/PostgreSQL/*/lib/ms
-  $ENV{ProgramW6432}/PostgreSQL/*/lib/ms
-  $ENV{SystemDrive}/PostgreSQL/*/lib/ms)
-
-if(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
-  set(POSTGRESQL_FOUND TRUE)
-else()
-  set(POSTGRESQL_FOUND FALSE)
-endif()
-
-# Handle the QUIETLY and REQUIRED arguments and set POSTGRESQL_FOUND to TRUE
-# if all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PostgreSQL
-  DEFAULT_MSG
-  POSTGRESQL_INCLUDE_DIR
-  POSTGRESQL_LIBRARIES
-  POSTGRESQL_VERSION)
-
-mark_as_advanced(POSTGRESQL_INCLUDE_DIR POSTGRESQL_LIBRARIES)
+MARK_AS_ADVANCED(POSTGRESQL_INCLUDE_DIRS POSTGRESQL_LIBRARIES)
